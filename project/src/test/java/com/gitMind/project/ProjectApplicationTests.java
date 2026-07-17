b1a1fce42f30cd8b1f37cd9c72221f0afeeff87d
ErSufiyan <Sufiyanamravati@gmail.com>
2026-07-18 01:31:40 +0530

    Initial commit

---
+++ project/src/test/java/com/gitMind/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitMind.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
